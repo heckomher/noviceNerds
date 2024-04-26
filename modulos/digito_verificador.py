a6d50fe8df74d8b55035c8e0196a99625cde1920
Hector Komori <hector.komori@gmail.com>
2024-04-26 19:43:20 -0400

    Se añade control de excepciones

--- modulos/digito_verificador.py
+++ modulos/digito_verificador.py
@@ -1,22 +1,31 @@
+def get_checker(value: int) -> str:
+    options = {10: "K", 11: "0"}
+    return options.get(value, value)
+
 def calcular_dv(number) -> str:
-    number_lenght = int(len(number))
-    array_number = [int(num) for num in number]
-    # Solución poco elegante: multiplicador = [2,3,4,5,6,7,2,3,4,5,6,7]
-    array_number.reverse()
-    multplier = 2
-    sum = 0
-    for i in range(number_lenght):
-        sum += array_number[i] * multplier #En la solucion poco elegante se pone multiplicador[i]
-        if multplier < 7:
+    try:
+        number_lenght = int(len(number))
+        array_number = [int(num) for num in number]
+        # Solución poco elegante: multiplicador = [2,3,4,5,6,7,2,3,4,5,6,7]
+        array_number.reverse()
+        multplier = 2
+        sum = 0
+        for i in range(number_lenght):
+            sum += array_number[i] * multplier #En la solucion poco elegante se pone multiplicador[i]
             multplier += 1
-        else:
-            multplier = 2
-    verifyer = 11 - (sum % 11)
-    if verifyer == 11:
-        return 0
-    elif verifyer == 10:
-        return 'K'
-    return verifyer
+            if multplier > 7:
+                multplier = 2
+        value = 11 - (sum % 11)
+        return get_checker(value)
+    except ValueError:
+        print("ERROR: El rut ingresado debe ser solo números")
+    except:
+        print("HA OCURRIDO UN ERROR INESPERADO")
+    #finally:
+    #    "cerrar conexión, por ejemplo"
 
 def validar(rut):
-    separar = rut.split("-")
+    split = rut.split("-")
+    checker = calcular_dv(split[0])
+    return str(checker) == str(split[1]).upper()
+        
